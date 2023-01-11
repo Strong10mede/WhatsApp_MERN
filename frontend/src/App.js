9c02405ce36968df26d776b2a05129726a3013a5
Strong10mede <kmayur809@gmail.com>
2023-01-11 16:45:44 +0530

    frontend done

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div>
+    </div>
+  );
+}
+
+export default App;
